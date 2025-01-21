439d4784983303ae3483d731d7834f641e0fe987
sayful-hassan <sayf.fashion@gmail.com>
2025-01-21 21:04:57 +0530

    Aggregate method is created to add products to cart

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,7 @@
 let db = require('../config/connection')
 let collection = require('../config/collections')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const { response } = require('../app');
 let objectId=require('mongodb').ObjectID
 
 
@@ -42,6 +43,15 @@ module.exports = {
         return new Promise(async(resolve,reject)=>{
             let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
             if(userCart){
+              db.get().collection(collection.CART_COLLECTION).updateOne({user:objectId(userId)},
+                {
+                    
+                        $push:{products:objectId(productId)}
+                    
+                }
+            ).then((response)=>{
+                resolve()
+            })
 
             }else{
                 let cartObj={
@@ -52,6 +62,32 @@ module.exports = {
                     resolve()
                 })
             }
+        })
+    },
+    getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems=await db.get().collection(collection.CART_COLLECTION).aggregate([
+                {
+                    $match:{user:objectId(userId)}
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTION,
+                        let:{productList:'$products'},
+                        pipeline:[
+                            {
+                               $match:{
+                                $expr:{
+                                    $in:['$_id','$$productList']
+                                }
+                               }
+                            }
+                        ],
+                        as:'cartItems'
+                    }
+                }
+            ]).toArray()
+            resolve(cartItems[0].cartItems)
         })
     }
 };
